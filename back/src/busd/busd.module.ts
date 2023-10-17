82579dbfe44bb1b4144b4f3d23dcff8abc0240fc
pooet <pooetitu@gmail.com>
2023-10-17 10:38:42 +0200

    Add volume clear

--- back/src/busd/busd.module.ts
+++ back/src/busd/busd.module.ts
@@ -5,11 +5,13 @@ import { TransferEntity } from './transfer.entity';
 import { BusdService } from './busd.service';
 import { BusdController } from './busd.controller';
 import { ContractModule } from '../contract/contract.module';
+import {VolumeModule} from "../volume/volume.module";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([AllowanceEntity, TransferEntity]),
     ContractModule,
+    VolumeModule,
   ],
   providers: [BusdService],
   controllers: [BusdController],
